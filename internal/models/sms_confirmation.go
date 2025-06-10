a1da1372eba2123885615a15877df3cc6c6fc9d5
Maral Kuanysh <230089@astanait.edu.kz>
2025-06-10 11:15:09 +0500

    Update sms_confirmation.go

--- internal/models/sms_confirmation.go
+++ internal/models/sms_confirmation.go
@@ -1,13 +1,12 @@
 package models
-import(
-  "time"
-)
 
-type SMS_CONFIRMATIONS struct{
-  ID int64 'json:"id"'
-  document_id int64 'json:"document_id"'
-  sms_code string 'json:"sms_code"'
-  sent_at time.Time 'json:"sent_at"'
-  confirmed bool 'json:"confirmed"'
-  confirmed_at time.Time 'json:"confirmed_at"'
+import "time"
+
+type SMSConfirmation struct {
+    ID          int64     `json:"id"`
+    DocumentID  int64     `json:"document_id"`
+    SMSCode     string    `json:"sms_code"`
+    SentAt      time.Time `json:"sent_at"`
+    Confirmed   bool      `json:"confirmed"`
+    ConfirmedAt time.Time `json:"confirmed_at"`
 }
