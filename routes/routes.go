f398e353242f31248e71aee491d9804d359baa3b
KUANAIT <153756396+KUANAIT@users.noreply.github.com>
2025-06-10 09:37:09 +0500

    routes

---
+++ routes/routes.go
@@ -0,0 +1,6 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin" //may be changed later if we will not going to use gin
+	"KUB/internal/handlers" //may be changed later after fully creating go mod file
+)
