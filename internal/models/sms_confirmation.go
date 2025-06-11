e9ec06371dcd960978c5867a98945ec608084d8e
Maral Kuanysh <230089@astanait.edu.kz>
2025-06-11 18:09:10 +0500

    Update sms_confirmation.go
    добавил дополнительно phone для реализации логики

--- internal/models/sms_confirmation.go
+++ internal/models/sms_confirmation.go
@@ -3,10 +3,11 @@ package models
 import "time"
 
 type SMSConfirmation struct {
-    ID          int64     `json:"id"`
-    DocumentID  int64     `json:"document_id"`
-    SMSCode     string    `json:"sms_code"`
-    SentAt      time.Time `json:"sent_at"`
-    Confirmed   bool      `json:"confirmed"`
-    ConfirmedAt time.Time `json:"confirmed_at"`
+	ID          int64     `json:"id"`
+	DocumentID  int64     `json:"document_id"`
+	Phone       string    `json:"phone"`
+	SMSCode     string    `json:"sms_code"`
+	SentAt      time.Time `json:"sent_at"`
+	Confirmed   bool      `json:"confirmed"`
+	ConfirmedAt time.Time `json:"confirmed_at"`
 }
