872f56ea1b0105ba7b0f16a07516c0d1b658fba6
BekzatS8 <159690965+BekzatS8@users.noreply.github.com>
2025-06-12 15:47:09 +0500

    add routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,2 +1,42 @@
 package routes
 
+import (
+	"database/sql"
+	"turcompany/internal/handlers"
+	"turcompany/internal/repositories"
+	"turcompany/internal/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(db *sql.DB) *gin.Engine {
+	r := gin.Default()
+
+	leadRepo := repositories.NewLeadRepository(db)
+	leadService := services.NewLeadService(leadRepo)
+	leadHandler := handlers.NewLeadHandler(leadService)
+	// Lead routes
+	leads := r.Group("/leads")
+	{
+		leads.POST("/", leadHandler.Create)
+		leads.GET("/:id", leadHandler.GetByID)
+		leads.PUT("/:id", leadHandler.Update)
+		leads.DELETE("/:id", leadHandler.Delete)
+	}
+
+	dealRepo := repositories.NewDealRepository(db)
+	dealService := services.NewDealService(dealRepo)
+	dealHandler := handlers.NewDealHandler(dealService)
+	//Deal routes
+	deals := r.Group("/deals")
+	{
+		deals.POST("/", dealHandler.Create)
+		deals.GET("/:id", dealHandler.GetByID)
+		deals.PUT("/:id", dealHandler.Update)
+		deals.DELETE("/:id", dealHandler.Delete)
+	}
+
+
+	
+	return r
+}
